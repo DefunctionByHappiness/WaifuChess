830f1428ed05b20b23dac4fa36aa76550a722dcb
Javier Rodríguez <chingowamen@hotmail.com>
2020-02-27 22:49:31 +0100

    - Initial structure for Loader, GameManager and BoardManage

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public int columns = 8;
+    public int rows = 8;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    public GameObject whiteTiles;
+    public GameObject blackTiles;
+
+    void InitializeList() {
+        gridPositions.Clear();
+
+        for (int x = 0; x < rows; x++)
+        {
+            for (int y = 0; y < columns; y++)
+            {   
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    void BoardSetup() {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = -1; x < rows + 1; x++)
+        {
+            for (int y = -1; y < columns + 1; y++)
+            {
+                /*
+                GameObject toInstantiate  = floorTiles[Random.Range(0, floorTiles.Length)];
+                if(x == -1 || x == rows || y == -1 || y == columns) 
+                    toInstantiate = outerWallTiles[Random.Range (0, outerWallTiles.Length)];
+                GameObject instance = Instantiate(toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);   
+                */   
+            }
+        }
+    }
+
+    public void SetupScene(){
+        BoardSetup();
+        InitializeList();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private BoardManager boardScript;                        //Store a reference to our BoardManager which will set up the level.
+    public static GameManager instance = null;                //Static instance of GameManager which allows it to be accessed by any other script.
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);    
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //enemies = new List<Enemy>();
+
+        //Get a component reference to the attached BoardManager script
+        boardScript = GetComponent<BoardManager>();
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver() {
+        //TODO: Setup GameOver logic
+        enabled = false;
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+        //TODO: Clear Chess pieces
+        //enemies.Clear();
+        //Call the SetupScene function of the BoardManager script, pass it current level number.
+        boardScript.SetupScene();
+
+
+    }
+}
